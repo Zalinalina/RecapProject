8dddaea8ddf7e5ad100972f406b0cd606bf9ab10
Zalina <Zalinasoria@gmail.com>
2021-06-30 20:25:47 -0400

    User story 101 has been completed

--- src/test/java/UserStory101.java
+++ src/test/java/UserStory101.java
@@ -1,6 +1,8 @@
 public class UserStory101 {
     public static void main(String[] args) {
         System.out.println("User story  ");
+        //has been done here, and ready to push to remote
+        //awesome work
     }
 
 }
